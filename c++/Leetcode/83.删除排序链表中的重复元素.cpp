1a9afdbad702e029e08d996d85e6e3258d9777cf
Jolson <jolsonz123@gmail.com>
2020-04-15 10:51:41 +0800

    4.15

---
+++ c++/Leetcode/83.删除排序链表中的重复元素.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=cpp
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}//这是构造函数把。
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+    }
+};
+// @lc code=end
+
