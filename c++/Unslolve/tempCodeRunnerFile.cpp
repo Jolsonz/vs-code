3c10a6dc44ae5943173435d83297a4375dc09a25
Jolson <jolsonz123@gmail.com>
2020-04-18 09:43:49 +0800

    删除

--- c++/Unslolve/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-    string longestCommonPrefix(vector<string>& strs) {
-        string ans;
-        sort(strs.begin(),strs.end());
-        for(int i=0;;i++){
-            if(strs[0][i]!=strs[strs.size()-1][i]) return strs[0].substr(0,i);
-        }
-    }
