0ae74cbb47f906d0ad1c91b8fc106e21b2959cc7
Jolson <jolsonz123@gmail.com>
2020-05-21 17:34:11 +0800

    study

--- Java/Puppy.java
+++ Java/Puppy.java
@@ -1,6 +1,6 @@
 public class Puppy{
    int puppyAge;//成员变量
-   public Puppy(String name){
+   public Puppy(String name){                                   
       // 这个构造器仅有一个参数：name
       System.out.println("小狗的名字是 : " + name ); 
    }
