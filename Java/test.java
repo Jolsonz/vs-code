d90c2529862297b1643ad1ab0bfbf3f7d4350ada
Jolson <jolsonz123@gmail.com>
2020-06-08 11:43:31 +0800

    6.8

--- Java/test.java
+++ Java/test.java
@@ -1,6 +1,21 @@
 public class test {
-    public static void main(String args[]) {
-        System.out.println("Hello World");
+    public static void main(String args[]){
+       int i = 1;
+       switch(i){
+          case 0:
+             System.out.println("0");
+             System.out.println("hello");
+             break;
+          case 1:
+             System.out.println("1");
+             break;
+          case 2:
+             System.out.println("2");
+          case 3:
+             System.out.println("3"); break;
+          default:
+             System.out.println("default");
+       }
     }
-}
+ }
  
--- c++/Unslolve/test.cpp
+++ c++/Unslolve/test.cpp
@@ -1,48 +1,14 @@
-#include <iostream>
-#include <set>
-#include <iterator>
-#include <algorithm>
-
-using namespace std;
-//求两个集合的和(并集)、差、交集
-//set库提供set容器，iterator库提供迭代器，algorithm库提供集合操作(故也可以对其他容器进行集合操作)
+#include<stdio.h>
+#include<windows.h>
 int main()
 {
-    set<int> a, b;
-    set<int> c, d, e;
-    a.insert(1);
-    a.insert(2);
-    b.insert(2);
-    b.insert(3);
-
-    set_union(a.begin(), a.end(), b.begin(), b.end(), insert_iterator<set<int>>(c, c.begin()));
-    set_difference(a.begin(), a.end(), b.begin(), b.end(), insert_iterator<set<int>>(d,d.begin()));
-    set_intersection(a.begin(), a.end(), b.begin(), b.end(), insert_iterator<set<int>>(e, e.begin()));
-
-    cout << "a: ";
-    for(auto i : a)
-        cout << i << " ";
-    cout << endl;
-
-    cout << "b: ";
-    for(auto i : b)
-        cout << i << " ";
-    cout << endl;
-
-    cout << "c = a + b: ";
-    for (auto i : c)
-        cout << i << " ";
-    cout << endl;
-
-    cout << "d = a - b: ";
-    for(auto i : d)
-        cout << i << " ";
-    cout << endl;
-
-    cout << "e = a & b: ";
-    for(auto i : e)
-        cout << i << " ";
-    cout << endl;
-
+    HANDLE hOut;
+    COORD pos={15,5};
+    hOut=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hOut,pos);
+     
+    SetConsoleTextAttribute(hOut,0x01|0x05);
+     
+    printf("HelloWorld!\n");
     return 0;
 }
